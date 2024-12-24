a1777b10435d1a45b881af66dc78f4f8116112f7
joo9810 <joo981001@kmu.kr>
2024-12-24 21:09:59 +0900

    java study

---
+++ src/Chap_06/_03_Return.java
@@ -0,0 +1,16 @@
+package Chap_06;
+
+public class _03_Return {
+    // 호텔 전화번호
+    public static String getPhoneNumber() {}
+
+    // 호텔 주소
+
+    // 호텔 액티비티
+
+    public static void
+
+    public static void main(String[] args) {
+        // 반환값, Return
+    }
+}
